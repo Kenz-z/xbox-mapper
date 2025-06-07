f14ee4c4f8a0fecd6ab5b9b9a78c2e2b5a87ee8c
Kenz-z <k.q.nttn.14@gmail.com>
2025-06-07 20:29:25 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,2 +1,11 @@
-// Placeholder content for App.js
-...
+import React from 'react';
+import { View, Text } from 'react-native';
+import GestureMappingScreen from './GestureMappingScreen';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1 }}>
+      <GestureMappingScreen />
+    </View>
+  );
+}
